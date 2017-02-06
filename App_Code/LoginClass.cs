6ae9e6157a3d0b1079e9f386b562e192add26080
jgilme01 <jgilme01@seattlecentral.edu>
2017-02-05 22:10:25 -0800

    ITC172 Wn17 Assn3

---
+++ App_Code/LoginClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data; 
+using System.Data.SqlClient; 
+using System.Configuration;
+
+/// <summary>
+/// Summary description for LoginClass
+/// </summary>
+public class LoginClass
+{
+    SqlConnection connect;
+    string email;
+    string pass;
+    public LoginClass(string userEmail, string password)
+    {
+        email = userEmail;
+        pass = password;
+        string connectString = ConfigurationManager.ConnectionStrings["Community_Assist_Connection"].ToString();
+        connect = new SqlConnection(connectString);
+    }
+
+    public int ValidateLogin()
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = connect;
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.CommandText = "usp_Login";
+        cmd.Parameters.AddWithValue("@email", email);
+        cmd.Parameters.AddWithValue("@password", pass);
+        connect.Open();
+        int result = cmd.ExecuteNonQuery();
+        connect.Close();
+        int key = 0;
+        if (result != -1)
+        {
+            key = GetUserKey();
+        }
+        return key;
+    }
+
+    private int GetUserKey()
+    {
+        string sql = "Select PersonKey from Person where PersonEmail=@email";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        cmd.Parameters.AddWithValue("@email", email);
+        SqlDataReader reader = null;
+        connect.Open();
+        reader = cmd.ExecuteReader();
+        int key = 0;
+        while (reader.Read())
+        {
+            key = (int)reader["PersonKey"];
+        }
+        connect.Close();
+        return key;
+    }
+}
